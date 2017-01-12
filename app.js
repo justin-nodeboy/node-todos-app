64f1b4c45d4c3c0d92bb633d33634ff9f37fe33c
Justin Howard <justin@justinhoward.co.uk>
2017-01-12 13:01:07 +0000

    Fixed mongo connection

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require('body-parser');
-const TodoRouter = require("./routes/TodoRouter");
+
 const MongoClient = require("./MongoClient");
 const app = module.exports = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(TodoRouter);
 
 MongoClient.prototype.connectDB(err => {
     if (err) process.exit();
+    const TodoRouter = require("./routes/TodoRouter");
+    app.use(TodoRouter);
     app.listen(process.env.PORT || 5000, function () {
         console.log('Server Listen');
     });
